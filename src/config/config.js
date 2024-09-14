0baf93e0d6cb13025b34e2e1aac3bf315c5aad5d
JoaquinB3 <joaquinbotteri3@gmail.com>
2024-09-14 18:49:36 -0300

    entrega final

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
+import 'dotenv/config'
 
 export const config = {
     PORT: 8080,
-    MONGO_URL: "mongodb+srv://joaquinbotteri:joaquinbotteri@cluster0.amlvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    MONGO_URL: `mongodb+srv://joaquinbotteri:${process.env.MONGODB_PASS}@cluster0.amlvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     DB_NAME: "tecnoDB"
 }
